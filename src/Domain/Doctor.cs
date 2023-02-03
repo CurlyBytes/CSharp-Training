3a8b83d87fc2208c014f69562bd52b62c220010a
Francisco Abayon <franz.noyaba@gmail.com>
2023-02-04 01:31:54 +0800

    feat(domain): doctor class with test

---
+++ src/Domain/Doctor.cs
@@ -0,0 +1,95 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Doctor : Employee
+    {
+        private string specialization;
+
+        public Doctor() { }
+
+        public Doctor(int employeeId, string name, string specialization) : base(employeeId, name)
+        {
+            this.Specialization = specialization;
+        }
+
+        public string Specialization { get => specialization; set => specialization = value; }
+
+        public override double CalculateDeduction()
+        {
+            double salary = base.GetSalary();
+            double sssDeduction = 0;
+            double pagibigDeduction = 0;
+            double wtaxDeduction = 0;
+            double philHealthDeduction = 0;
+
+            if (salary <= 10000)
+            {
+                sssDeduction = salary * 0.03;
+                pagibigDeduction = salary * 0.02;
+                wtaxDeduction = salary * 0.05;
+                philHealthDeduction = salary * 0.01;
+            }
+            else if (salary > 10000 && salary <= 20000)
+            {
+                sssDeduction = salary * 0.05;
+                pagibigDeduction = salary * 0.04;
+                wtaxDeduction = salary * 0.10;
+                philHealthDeduction = salary * 0.025;
+            }
+            else if (salary > 20000 && salary <= 30000)
+            {
+                sssDeduction = salary * 0.07;
+                pagibigDeduction = salary * 0.07;
+                wtaxDeduction = salary * 0.15;
+                philHealthDeduction = salary * 0.05;
+            }
+            else if (salary > 30000)
+            {
+                sssDeduction = salary * 0.10;
+                pagibigDeduction = salary * 0.10;
+                wtaxDeduction = salary * 0.30;
+                philHealthDeduction = salary * 0.08;
+            }
+
+            double totalDeduction = sssDeduction + pagibigDeduction + wtaxDeduction + philHealthDeduction;
+
+            return totalDeduction;
+        }
+
+        public override double CalculateNetPay()
+        {
+            return base.GetSalary() - CalculateDeduction();
+        }
+
+        public override double CalculateSalary(double numberOfDaysWorked)
+        {
+            double rate = 0;
+            switch (Specialization)
+            {
+                case "Pediatrician":
+                    rate = 2050;
+                    break;
+                case "Ob-Gynecologist":
+                    rate = 2650;
+                    break;
+                case "Neurologist":
+                    rate = 6575;
+                    break;
+            }
+
+      
+            base.SetSalary(rate * numberOfDaysWorked);
+            return base.GetSalary();
+        }
+
+        public new void Display()
+        {
+            base.Display();
+            Console.WriteLine("Specialization " + Specialization);
+        }
+    }
+}
---
+++ tests/DomainUnitTest/DoctorTests.cs
@@ -0,0 +1,55 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DomainUnitTest
+{
+    public class DoctorTest
+    {
+        [Fact]
+        public void TestConstructor()
+        {
+            Doctor doctor = new Doctor(001, "Juan dela Cruz", "Pediatrician");
+            Assert.Equal(1, doctor.GetEmployeeId());
+            Assert.Equal("Juan dela Cruz", doctor.GetName());
+            Assert.Equal("Pediatrician", doctor.Specialization);
+        }
+
+        [Fact]
+        public void TestSetSpecialization()
+        {
+            Doctor doctor = new Doctor();
+            doctor.Specialization = "Ob-Gynecologist";
+            Assert.Equal("Ob-Gynecologist", doctor.Specialization);
+        }
+
+        [Fact]
+        public void TestCalculateSalary()
+        {
+            Doctor doctor = new Doctor(001, "Juan dela Cruz", "Pediatrician");
+            double salary = doctor.CalculateSalary(15);
+            Assert.Equal(30750, salary, 2);
+        }
+
+        [Fact]
+        public void TestCalculateDeduction()
+        {
+            Doctor doctor = new Doctor(001, "Juan dela Cruz", "Pediatrician");
+            double salary = doctor.CalculateSalary(15);
+            double deduction = doctor.CalculateDeduction();
+            Assert.Equal(17835, deduction, 2);
+        }
+
+        [Fact]
+        public void TestCalculateNetPay()
+        {
+            Doctor doctor = new Doctor(001, "Juan dela Cruz", "Pediatrician");
+            double salary = doctor.CalculateSalary(15);
+            double deduction = doctor.CalculateDeduction();
+            double netPay = doctor.CalculateNetPay();
+            Assert.Equal(12915, netPay, 2);
+        }
+    }
+}
